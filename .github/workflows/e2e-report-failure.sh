e2b0b0d9876d53f4a331f5e1cd4b17b41bc298e6
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-28 09:36:53 -0700

    Update e2e-report-failure.sh

--- .github/workflows/e2e-report-failure.sh
+++ .github/workflows/e2e-report-failure.sh
@@ -14,11 +14,14 @@ Branch: $GITHUB_REF_NAME
 Date: $RUN_DATE
 EOF
 
+echo ISSUE_ID=$(gh -R "$TARGET_REPOSITORY" issue list --state open -S "$THIS_FILE" --json number | jq '.[0]' | jq -r '.number')
 ISSUE_ID=$(gh -R "$TARGET_REPOSITORY" issue list --state open -S "$THIS_FILE" --json number | jq '.[0]' | jq -r '.number')
 
 if [[ -z "$ISSUE_ID" ]]; then
+  echo gh -R "$TARGET_REPOSITORY" issue create -t "BUG: $GITHUB_WORKFLOW" -F ./BODY
   gh -R "$TARGET_REPOSITORY" issue create -t "BUG: $GITHUB_WORKFLOW" -F ./BODY
 else
+  echo gh -R "$TARGET_REPOSITORY" issue comment "$ISSUE_ID" -F ./BODY
   gh -R "$TARGET_REPOSITORY" issue comment "$ISSUE_ID" -F ./BODY
 fi
 
