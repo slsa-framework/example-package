adb5f83a73ab99fa4633697bcd78dae2bee7b720
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-28 15:32:32 -0700

    Update e2e-push.sh

--- .github/workflows/scripts/e2e-push.sh
+++ .github/workflows/scripts/e2e-push.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# We push to main a file e2e/wokflow-name.txt
+# with the date inside.
+
+DATE=$(date --utc)
+FILE=e2e/$THIS_FILE.txt
+echo $DATE > $FILE
+
+COMMIT_MESSAGE="E2e push: $GITHUB_WORKFLOW"
+
+# git config --global user.name github-actions
+# git config --global user.email github-actions@github.com
+# git add $FILE
+# git commit -m "E2e push: $GITHUB_WORKFLOW"
+# git push
+          
 # https://docs.github.com/en/rest/repos/contents#create-a-file.
 curl \
   -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GH_TOKEN" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/contents/e2e/$FILE \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$FILE \
   -d '{"message":"$COMMIT_MESSAGE","committer":{"name":"github-actions","email":"github-actions@github.com"},"content":"$(cat $DATE | base64 --wrap=0)"}'
