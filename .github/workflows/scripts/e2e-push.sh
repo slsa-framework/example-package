817c235b356a81a5ae22793ad2340043a089e1d9
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-28 16:06:44 -0700

    Update e2e-push.sh

--- .github/workflows/scripts/e2e-push.sh
+++ .github/workflows/scripts/e2e-push.sh
@@ -5,9 +5,34 @@
 
 DATE=$(date --utc)
 FILE=e2e/$THIS_FILE.txt
-echo $DATE > $FILE
-
 COMMIT_MESSAGE="E2e push $GITHUB_WORKFLOW"
+
+if [[ -f "$FILE" ]]; then
+  # sha of existing file.
+  SHA=$(cat "$FILE" | sha1sum | cut -d " " -f1)
+  
+  echo $DATE > $FILE
+  
+  # https://docs.github.com/en/rest/repos/contents#create-a-file.
+  curl \
+    -X PUT \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $GH_TOKEN" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$FILE \
+    -d '{"message":"$COMMIT_MESSAGE","sha":"$SHA","committer":{"name":"github-actions","email":"github-actions@github.com"},"content":"$(cat $DATE | base64 --wrap=0)"}'
+else
+  echo $DATE > $FILE
+  
+  # https://docs.github.com/en/rest/repos/contents#create-a-file.
+  curl \
+    -X PUT \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $GH_TOKEN" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$FILE \
+    -d '{"message":"$COMMIT_MESSAGE","committer":{"name":"github-actions","email":"github-actions@github.com"},"content":"$(cat $DATE | base64 --wrap=0)"}'
+fi
+
+
 
 # git config --global user.name github-actions
 # git config --global user.email github-actions@github.com
@@ -15,10 +40,3 @@ COMMIT_MESSAGE="E2e push $GITHUB_WORKFLOW"
 # git commit -m "E2e push: $GITHUB_WORKFLOW"
 # git push
           
-# https://docs.github.com/en/rest/repos/contents#create-a-file.
-curl \
-  -X PUT \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $GH_TOKEN" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$FILE \
-  -d '{"message":"$COMMIT_MESSAGE","committer":{"name":"github-actions","email":"github-actions@github.com"},"content":"$(cat $DATE | base64 --wrap=0)"}'
