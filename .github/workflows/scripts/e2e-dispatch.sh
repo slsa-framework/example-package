231a1a4d0c7ff62531aac6a161cfde139b29015c
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-28 17:55:28 -0700

    Update e2e-dispatch.sh

--- .github/workflows/scripts/e2e-dispatch.sh
+++ .github/workflows/scripts/e2e-dispatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST -H "Accept: application/vnd.github.v3+json" \
+curl -s -X POST -H "Accept: application/vnd.github.v3+json" \
      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$THIS_FILE/dispatches \
      -d '{"ref":"main"}' \
      -H "Authorization: token $GH_TOKEN"
