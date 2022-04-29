939724dff0dccb6e7b7dd3cdb2333d5da6789805
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-04-28 18:05:42 -0700

    Update e2e-verify.sh

--- .github/workflows/scripts/e2e-verify.sh
+++ .github/workflows/scripts/e2e-verify.sh
@@ -9,23 +9,23 @@ go install github.com/slsa-framework/slsa-verifier@latest
     
 # Default parameters.
 RES=$(slsa-verifier --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_eq($RES, 0)
+assert_eq "$RES" "0"
 
 # Main branch
 RES=$(slsa-verifier --branch main --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_eq($RES, 0)
+assert_eq "$RES" "0"
 
 # Wrong branch
 RES=$(slsa-verifier --branch not-main --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_not_eq($RES, 0)
+assert_not_eq "$RES" "0"
 
 # Wrong tag
 RES=$(slsa-verifier --tag v1.2.3 --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_not_eq($RES, 0)
+assert_not_eq "$RES" "0"
 
 # Wrong versioned-tag
 RES=$(slsa-verifier --versioned-tag v1.2.3 --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_not_eq($RES, 0)
+assert_not_eq "$RES" "0"
 
 #TODO: read out the provenance information once we print it
 #TODO: previous releases, curl the $BINARY directly. We should list the releases and run all commands automatically
