25fccd17b6192ea2fe886af74aace89430f837a6
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-05-03 09:29:08 -0700

    Update e2e-verify.sh

--- .github/workflows/scripts/e2e-verify.sh
+++ .github/workflows/scripts/e2e-verify.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-source "./.github/workflows/scripts/assert.sh"
-
-exit_if_fail() {
-    local actual="$1"
-    if [ "$actual" != "0" ]; then
-        exit 1
-    fi
-}
+source "./.github/workflows/scripts/e2e-assert.sh"
 
 go env -w GOFLAGS=-mod=mod
 
@@ -16,28 +9,23 @@ go install github.com/slsa-framework/slsa-verifier@latest
     
 # Default parameters.
 RES=$(slsa-verifier --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_eq "$RES" "0" "default parameters"
-exit_if_fail "$?"
+e2e_assert_eq "$RES" "0" "default parameters"
 
 # Main branch
 RES=$(slsa-verifier --branch main --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_eq "$RES" "0" "main branch"
-exit_if_fail "$?"
+e2e_assert_eq "$RES" "0" "main branch"
 
 # Wrong branch
 RES=$(slsa-verifier --branch not-main --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_not_eq "$RES" "0" "wrong branch"
-exit_if_fail "$?"
+e2e_assert_not_eq "$RES" "0" "wrong branch"
 
 # Wrong tag
 RES=$(slsa-verifier --tag v1.2.3 --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_not_eq "$RES" "0" "wrong tag"
-exit_if_fail "$?"
+e2e_assert_not_eq "$RES" "0" "wrong tag"
 
 # Wrong versioned-tag
 RES=$(slsa-verifier --versioned-tag v1.2.3 --artifact-path $BINARY --provenance $PROVENANCE --source github.com/$GITHUB_REPOSITORY)
-assert_not_eq "$RES" "0" "wrong versioned-tag"
-exit_if_fail "$?"
+e2e_assert_not_eq "$RES" "0" "wrong versioned-tag"
 
 #TODO: read out the provenance information once we print it
 #TODO: previous releases, curl the $BINARY directly. We should list the releases and run all commands automatically
