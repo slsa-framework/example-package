d2cef8137b194225cd7ac215f2a8568d621ed01e
laurentsimon <laurentsimon@google.com>
2022-05-03 20:26:50 +0000

    comments

--- .github/workflows/scripts/e2e-utils.sh
+++ .github/workflows/scripts/e2e-utils.sh
@@ -11,64 +11,64 @@ if [[ -z "$CONFIG_FILE" ]]; then
 fi
 
 e2e_verify_predicate_subject_name() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -r '.subject[0].name')
+    name=$(echo -n "$attestation" | jq -r '.subject[0].name')
     e2e_assert_eq "$name" "$expected" "subject should be $expected"
 }
 
 e2e_verify_predicate_builder_id() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -r '.predicate.builder'.id)
+    name=$(echo -n "$attestation" | jq -r '.predicate.builder'.id)
     e2e_assert_eq "$name" "$expected" "builder should be $expected"
 }
 
 e2e_verify_predicate_builderType() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -r '.predicate.buildType')
+    name=$(echo -n "$attestation" | jq -r '.predicate.buildType')
     e2e_assert_eq "$name" "$expected" "builderType should be $expected"
 }
 
 e2e_verify_predicate_invocation_configSource() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -c -r '.predicate.invocation.configSource')
+    name=$(echo -n "$attestation" | jq -c -r '.predicate.invocation.configSource')
     e2e_assert_eq "$name" "$expected" "configSource should be $expected"
 }
 
 e2e_verify_predicate_invocation_environment() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -r '.predicate.invocation.environment | .github_actor, .github_sha1, .os, .arch, .github_event_name, .github_ref, .github_ref_type' | jq -R -s -c 'split("\n") | map(select(length > 0))')
+    name=$(echo -n "$attestation" | jq -r '.predicate.invocation.environment | .github_actor, .github_sha1, .os, .arch, .github_event_name, .github_ref, .github_ref_type' | jq -R -s -c 'split("\n") | map(select(length > 0))')
     e2e_assert_eq "$name" "$expected" "environment should be $expected"
 }
 
 e2e_verify_predicate_buildConfig_command() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -c -r '.predicate.buildConfig.steps[0].command[1:]')
+    name=$(echo -n "$attestation" | jq -c -r '.predicate.buildConfig.steps[0].command[1:]')
     e2e_assert_eq "$name" "$expected" "command should be $expected"
 }
 
 e2e_verify_predicate_buildConfig_env() {
-    local provenance="$1"
-    local expected="$2"
-    name=$(cat $provenance | jq -c -r '.predicate.buildConfig.steps[0].env')
+    local attestation="$1"
+    local expected=$(echo -n "$2" | jq -c '.| sort')
+    name=$(echo -n "$attestation" | jq -c '.predicate.buildConfig.steps[0].env | sort')
     e2e_assert_eq "$name" "$expected" "env should be $expected"
 }
 
 e2e_verify_predicate_metadata() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -c -r '.predicate.metadata')
+    name=$(echo -n "$attestation" | jq -c -r '.predicate.metadata')
     e2e_assert_eq "$name" "$expected" "metadata should be $expected"
 }
 
 e2e_verify_predicate_materials() {
-    local provenance="$1"
+    local attestation="$1"
     local expected="$2"
-    name=$(cat $provenance | jq -c -r '.predicate.materials[0]')
+    name=$(echo -n "$attestation" | jq -c -r '.predicate.materials[0]')
     e2e_assert_eq "$name" "$expected" "material should be $expected"
 }
--- .github/workflows/scripts/e2e-verify.sh
+++ .github/workflows/scripts/e2e-verify.sh
@@ -28,22 +28,24 @@ source "./.github/workflows/scripts/e2e-utils.sh"
 # e2e_assert_not_eq "$RES" "0" "wrong versioned-tag"
 
 # Provenance content verification.
-TRIGGER=$(echo "$THIS_FILE" | cut -d '.' -f3)
-BRANCH=$(echo "$THIS_FILE" | cut -d '.' -f4)
-e2e_verify_predicate_subject_name "$PROVENANCE" "binary-linux-amd64"
-e2e_verify_predicate_builder_id "$PROVENANCE" "https://github.com/slsa-framework/slsa-github-generator-go/.github/workflows/slsa3_builder.yml@$BRANCH"
-e2e_verify_predicate_builderType "$PROVENANCE" "https://github.com/slsa-framework/slsa-github-generator-go@v1"
-e2e_verify_predicate_invocation_configSource "$PROVENANCE" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@refs/heads/$BRANCH\",\"digest\":{\"sha1\":\"$GITHUB_SHA\"},\"entryPoint\":\"$GITHUB_WORKFLOW\"}"
+ATTESTATION=$(cat "$PROVENANCE")
+TRIGGER=$(echo "$THIS_FILE" | cut -d '.' -f4)
+BRANCH=$(echo "$THIS_FILE" | cut -d '.' -f5)
+
+e2e_verify_predicate_subject_name "$ATTESTATION" "binary-linux-amd64"
+e2e_verify_predicate_builder_id "$ATTESTATION" "https://github.com/slsa-framework/slsa-github-generator-go/.github/workflows/slsa3_builder.yml@refs/heads/main"
+e2e_verify_predicate_builderType "$ATTESTATION" "https://github.com/slsa-framework/slsa-github-generator-go@v1"
+e2e_verify_predicate_invocation_configSource "$ATTESTATION" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@refs/heads/$BRANCH\",\"digest\":{\"sha1\":\"$GITHUB_SHA\"},\"entryPoint\":\"$GITHUB_WORKFLOW\"}"
 if [[ "$TRIGGER" != "tag" ]]; then
-    e2e_verify_predicate_invocation_environment "$PROVENANCE" "[\"$GITHUB_ACTOR\",\"$GITHUB_SHA\",\"ubuntu20\",\"X64\",\"$TRIGGER\",\"refs/heads/main\",\"branch\"]"
+    e2e_verify_predicate_invocation_environment "$ATTESTATION" "[\"$GITHUB_ACTOR\",\"$GITHUB_SHA\",\"ubuntu20\",\"X64\",\"$TRIGGER\",\"refs/heads/main\",\"branch\"]"
 else
-    e2e_verify_predicate_invocation_environment "$PROVENANCE" "[\"$GITHUB_ACTOR\",\"$GITHUB_SHA\",\"ubuntu20\",\"X64\",\"$TRIGGER\",\"refs/tags/main\",\"tag\"]"
+    e2e_verify_predicate_invocation_environment "$ATTESTATION" "[\"$GITHUB_ACTOR\",\"$GITHUB_SHA\",\"ubuntu20\",\"X64\",\"$TRIGGER\",\"refs/tags/main\",\"tag\"]"
 fi
 
-e2e_verify_predicate_buildConfig_command "$PROVENANCE" "[\"build\",\"-mod=vendor\",\"-trimpath\",\"-tags=netgo\",\"-o\",\"binary-linux-amd64\"]"
-e2e_verify_predicate_buildConfig_env "$PROVENANCE" "[\"GOOS=linux\",\"GOARCH=amd64\",\"GO111MODULE=on\",\"CGO_ENABLED=0\"]"
-e2e_verify_predicate_metadata "$PROVENANCE" "{\"completeness\":{\"parameters\":true,\"environment\":false,\"materials\":false},\"reproducible\":false}"
-e2e_verify_predicate_metadata "$PROVENANCE" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@refs/heads/$BRANCH\",\"digest\":{\"sha1\":\"$GITHUB_SHA\"}}"
+e2e_verify_predicate_buildConfig_command "$ATTESTATION" "[\"build\",\"-mod=vendor\",\"-trimpath\",\"-tags=netgo\",\"-o\",\"binary-linux-amd64\"]"
+e2e_verify_predicate_buildConfig_env "$ATTESTATION" "[\"GOOS=linux\",\"GOARCH=amd64\",\"GO111MODULE=on\",\"CGO_ENABLED=0\"]"
+e2e_verify_predicate_metadata "$ATTESTATION" "{\"completeness\":{\"parameters\":true,\"environment\":false,\"materials\":false},\"reproducible\":false}"
+e2e_verify_predicate_materials "$ATTESTATION" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@refs/heads/$BRANCH\",\"digest\":{\"sha1\":\"$GITHUB_SHA\"}}"
 
 #TODO: read out the provenance information once we print it
 #TODO: previous releases, curl the $BINARY directly. We should list the releases and run all commands automatically
