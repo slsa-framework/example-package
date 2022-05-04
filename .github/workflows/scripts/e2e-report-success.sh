2b02b387021cc3206a622ce1f92e3ddaa9d9b4cc
laurentsimon <laurentsimon@google.com>
2022-05-04 21:27:56 +0000

    updates

--- .github/workflows/scripts/e2e-report-success.sh
+++ .github/workflows/scripts/e2e-report-success.sh
@@ -7,7 +7,7 @@ e2e_create_issue_success_body
 ISSUE_ID=$(gh -R "$ISSUE_REPOSITORY" issue list --state open -S "$THIS_FILE" --json number | jq '.[0]' | jq -r '.number' | jq 'select (.!=null)')
 
 if [[ ! -z "$ISSUE_ID" ]]; then
-  gh -R "$ISSUE_REPOSITORY" issue close "$ISSUE_ID" -F ./BODY
+  gh -R "$ISSUE_REPOSITORY" issue close "$ISSUE_ID" -c $(cat ./BODY)
 fi
 
  
