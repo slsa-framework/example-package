0f4e32b366004569196268fdcb2a0313d9151c5d
laurentsimon <laurentsimon@google.com>
2022-05-06 18:58:33 +0000

    updates16

--- .github/workflows/scripts/e2e-verify-release.sh
+++ .github/workflows/scripts/e2e-verify-release.sh
@@ -10,9 +10,12 @@ fi
 # Verify that the release is intended for this e2e workflow
 #, ie that the notes contains the string $THIS_FILE
 TAG="$GITHUB_REF_NAME"
+echo "tag is $TAG"
 BODY=$(gh release view "$TAG" --json body | jq -r '.body')
 if [[ "$BODY" == *"$THIS_FILE"* ]]; then
     RELEASE_TAG="$TAG"
+    echo "we found it $RELEASE_TAG"
     echo "::set-output name=continue::yes"
 fi
 
+exit 10
