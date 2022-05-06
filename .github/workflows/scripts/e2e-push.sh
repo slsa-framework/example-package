64ca241a18c71f6dab23d9bac410704e41f98135
laurentsimon <laurentsimon@google.com>
2022-05-06 21:03:29 +0000

    updates34

--- .github/workflows/scripts/e2e-push.sh
+++ .github/workflows/scripts/e2e-push.sh
@@ -6,6 +6,7 @@
 DATE=$(date --utc)
 FILE=e2e/$THIS_FILE.txt
 COMMIT_MESSAGE="$GITHUB_WORKFLOW"
+BRANCH=$(echo "$THIS_FILE" | cut -d '.' -f4)
 
 if [[ -f "$FILE" ]]; then
   SHA=$(curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GH_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$FILE | jq -r '.sha')
@@ -14,7 +15,7 @@ if [[ -f "$FILE" ]]; then
 
   # Add the file content's sha to the request.
   cat << EOF > DATA
-{"message":"$COMMIT_MESSAGE","sha":"$SHA","committer":{"name":"github-actions","email":"github-actions@github.com"},"content":"$(echo -n $DATE | base64 --wrap=0)"}
+{"branch":"$BRANCH","message":"$COMMIT_MESSAGE","sha":"$SHA","committer":{"name":"github-actions","email":"github-actions@github.com"},"content":"$(echo -n $DATE | base64 --wrap=0)"}
 EOF
 
   # https://docs.github.com/en/rest/repos/contents#create-a-file.
