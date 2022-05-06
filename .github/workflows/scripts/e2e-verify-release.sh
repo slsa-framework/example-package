60c43380c57dd2d44a9c0a1fb53057a321992ad6
laurentsimon <laurentsimon@google.com>
2022-05-06 20:22:04 +0000

    updates27

--- .github/workflows/scripts/e2e-verify-release.sh
+++ .github/workflows/scripts/e2e-verify-release.sh
@@ -12,6 +12,7 @@ BRANCH=$(echo "$THIS_FILE" | cut -d '.' -f4)
 ENV_BRANCH=$(cat "$GITHUB_EVENT_PATH" | jq -r '.base_ref')
 
 echo "id: $GITHUB_RUN_ID-$GITHUB_RUN_ATTEMPT"
+echo "branch: $BRANCH"
 cat "$GITHUB_EVENT_PATH"
 echo
 
