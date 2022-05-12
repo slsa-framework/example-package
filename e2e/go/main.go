93d451024c5750f3d44041b5b688d5fbff7ff522
laurentsimon <laurentsimon@google.com>
2022-05-12 21:48:34 +0000

    update

---
+++ e2e/go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pborman/uuid"
+)
+
+var (
+	gitVersion string
+	gitCommit  string
+	gitBranch  string
+	gitMain    string
+)
+
+func main() {
+	uuidWithHyphen := uuid.NewRandom()
+	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
+
+	fmt.Println("GitBranch:", gitBranch)
+	fmt.Println("GitVersion:", gitVersion)
+	fmt.Println("GitCommit:", gitCommit)
+	fmt.Println("GitMain:", gitMain)
+	fmt.Println("Hello world:", uuid)
+}
