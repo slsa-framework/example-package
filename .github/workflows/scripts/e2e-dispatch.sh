986c0f4adb47b7c1f1635814c67080bb04358897
laurentsimon <laurentsimon@google.com>
2022-05-13 15:45:45 +0000

    update

--- .github/workflows/scripts/e2e-dispatch.sh
+++ .github/workflows/scripts/e2e-dispatch.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 BRANCH=$(echo "$THIS_FILE" | cut -d '.' -f4)
-echo curl -s -X POST -H "Accept: application/vnd.github.v3+json" \
-     https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$THIS_FILE/dispatches \
-     -d "{\"ref\":\"$BRANCH\"}" \
-     -H "Authorization: token $GH_TOKEN"
-
 curl -s -X POST -H "Accept: application/vnd.github.v3+json" \
      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$THIS_FILE/dispatches \
      -d "{\"ref\":\"$BRANCH\"}" \
