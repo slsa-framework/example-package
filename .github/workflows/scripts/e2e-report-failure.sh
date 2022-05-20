fafd080d0a3d115fdcaa857369d133ad10b1438d
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-05-19 17:08:30 -0700

    Update e2e-report-failure.sh

--- .github/workflows/scripts/e2e-report-failure.sh
+++ .github/workflows/scripts/e2e-report-failure.sh
@@ -7,7 +7,7 @@ e2e_create_issue_failure_body
 ISSUE_ID=$(gh -R "$ISSUE_REPOSITORY" issue list --state open -S "$THIS_FILE" --json number | jq '.[0]' | jq -r '.number' | jq 'select (.!=null)')
 
 if [[ -z "$ISSUE_ID" ]]; then
-  gh -R "$ISSUE_REPOSITORY" issue create -t "E2E: $GITHUB_WORKFLOW" -F ./BODY -l e2e -l bug
+  gh -R "$ISSUE_REPOSITORY" issue create -t "E2E: $GITHUB_WORKFLOW" -F ./BODY -l e2e -l "type:bug"
 else
   gh -R "$ISSUE_REPOSITORY" issue comment "$ISSUE_ID" -F ./BODY
 fi
