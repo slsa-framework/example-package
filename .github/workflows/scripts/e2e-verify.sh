34a1ccf6371b4abd68764ddc6e0e8e9d1013081c
laurentsimon <laurentsimon@google.com>
2022-05-23 16:39:15 +0000

    updates

--- .github/workflows/scripts/e2e-verify.sh
+++ .github/workflows/scripts/e2e-verify.sh
@@ -140,8 +140,10 @@ LDFLAGS=$(echo "$THIS_FILE" | cut -d '.' -f5 | grep -v noldflags)
 ASSETS=$(echo "$THIS_FILE" | cut -d '.' -f5 | grep -v noassets)
 # Note GO_MAIN and GO_DIR are set in the workflows as env variables.
 DIR="$PWD"
+echo "$DIR"
 if [[ -n "$GO_MAIN" ]]; then
     DIR="$DIR/$GO_MAIN"
+    echo "now DIR is $DIR"
 fi
 e2e_verify_predicate_subject_name "$ATTESTATION" "$BINARY"
 e2e_verify_predicate_builder_id "$ATTESTATION" "https://github.com/slsa-framework/slsa-github-generator/.github/workflows/builder_go_slsa3.yml@refs/heads/main"
