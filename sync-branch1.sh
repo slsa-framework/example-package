ab2f43f4076046c173d0a5c84064b9cf8de494c5
laurentsimon <laurentsimon@google.com>
2022-06-10 17:05:21 +0000

    updates

--- sync-branch1.sh
+++ sync-branch1.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -euo pipefail
+
+git pull --rebase
 
 git branch -D branch1
 git checkout -b branch1
 git push --set-upstream origin branch1 -f
+
 git checkout main
+
+# Update a dummy file to avoid https://github.com/slsa-framework/example-package/issues/44
+echo "$(date)" > ./e2e/dummy
+git add ./e2e/dummy
+git push
