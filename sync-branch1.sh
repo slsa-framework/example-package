e978bb3fe7d44ed4391ee7c9fd351eb04c607810
laurentsimon <laurentsimon@google.com>
2022-06-10 17:06:02 +0000

    updates

--- sync-branch1.sh
+++ sync-branch1.sh
@@ -10,6 +10,8 @@ git push --set-upstream origin branch1 -f
 git checkout main
 
 # Update a dummy file to avoid https://github.com/slsa-framework/example-package/issues/44
-echo "$(date)" > ./e2e/dummy
+DATE="$(date)"
+echo "$DATE" > ./e2e/dummy
 git add ./e2e/dummy
+git commit -m "sync'ing branch1 - $DATE"
 git push
