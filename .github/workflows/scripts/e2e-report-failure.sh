bb155dd9256e89092f69479c24111952f33fdd78
laurentsimon <laurentsimon@google.com>
2022-06-14 22:42:15 +0000

    updates

--- .github/workflows/scripts/e2e-report-failure.sh
+++ .github/workflows/scripts/e2e-report-failure.sh
@@ -10,7 +10,9 @@ e2e_create_issue_failure_body
 ISSUE_ID=$(gh -R "$ISSUE_REPOSITORY" issue list --state open -S "$THIS_FILE" --json number | jq '.[0]' | jq -r '.number' | jq 'select (.!=null)')
 
 if [[ -z "$ISSUE_ID" ]]; then
-    gh -R "$ISSUE_REPOSITORY" issue create -t "E2E: $GITHUB_WORKFLOW" -F ./BODY -l e2e -l "type:bug"
+    # Replace `.`` by ` ` and remove the last 3 characters `yml`
+    TITLE=$(echo "$THIS_FILE" | sed -e 's/\./ /g' | rev | cut -c4- | rev)
+    gh -R "$ISSUE_REPOSITORY" issue create -t "E2E: $TITLE" -F ./BODY -l e2e -l "type:bug"
 else
     gh -R "$ISSUE_REPOSITORY" issue comment "$ISSUE_ID" -F ./BODY
 fi
