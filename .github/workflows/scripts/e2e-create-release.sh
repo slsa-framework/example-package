ac07d5d397bd8f6f6152623d77c4743e6299abd5
Ian Lewis <ianmlewis@gmail.com>
2022-06-24 02:15:53 +0000

    Do full version comparisons

--- .github/workflows/scripts/e2e-create-release.sh
+++ .github/workflows/scripts/e2e-create-release.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 # shellcheck source=/dev/null
 source "./.github/workflows/scripts/e2e-utils.sh"
 
-RELEASE_TAG=""
-
 THIS_FILE=$(e2e_this_file)
 echo "THIS_FILE: $THIS_FILE"
 
@@ -18,35 +16,27 @@ fi
 
 # List the releases and find the latest for THIS_FILE.
 DEFAULT_MAJOR=$(version_major "$DEFAULT_VERSION")
+if [[ -z "$DEFAULT_MAJOR" ]]; then
+    echo "Invalid DEFAULT_VERSION: $DEFAULT_VERSION"
+    exit 1
+fi
+
+# Here we find the highest version with the major version equal to that of
+# DEFAULT_VERSION.
 RELEASE_LIST=$(gh release -L 200 list)
-HIGHEST_PATCH="0"
+HIGHEST_TAG=$DEFAULT_VERSION
 while read -r line; do
     TAG=$(echo "$line" | cut -f1)
     MAJOR=$(version_major "$TAG")
     if [ "$MAJOR" == "$DEFAULT_MAJOR" ]; then
-        # We only bump the patch, so we need not verify major/minor.
-        PATCH=$(version_patch "$TAG")
-        if [ "$PATCH" == "" ]; then
-            echo "invalid patch version for $TAG"
-            continue
-        fi
         echo "  Processing $TAG"
-        echo "  PATCH: $PATCH"
-        echo "  HIGEST_PATCH: $HIGHEST_PATCH"
-        echo "  RELEASE_TAG: $RELEASE_TAG"
-        if [[ "$PATCH" -gt "$HIGHEST_PATCH" ]]; then
+        echo "  HIGHEST_TAG: $HIGHEST_TAG"
+        if version_gt "$TAG" "$HIGHEST_TAG"; then
             echo " INFO: updating to $TAG"
-            PATCH="$P"
-            RELEASE_TAG="$TAG"
+            HIGHEST_TAG="$TAG"
         fi
     fi
 done <<<"$RELEASE_LIST"
-
-if [[ -z "$RELEASE_TAG" ]]; then
-    echo "Tag not found for $THIS_FILE"
-    echo "Defaulting to DEFAULT_VERSION: $DEFAULT_VERSION"
-    RELEASE_TAG="$DEFAULT_VERSION"
-fi
 
 echo "Latest tag found is $RELEASE_TAG"
 
@@ -54,17 +44,15 @@ RELEASE_MAJOR=$(version_major "$RELEASE_TAG")
 RELEASE_MINOR=$(version_minor "$RELEASE_TAG")
 RELEASE_PATCH=$(version_patch "$RELEASE_TAG")
 NEW_PATCH=$((${RELEASE_PATCH:-0} + 1))
-NEW_RELEASE_TAG="${RELEASE_MAJOR:-$DEFAULT_MAJOR}.${RELEASE_MINOR:-0}.$NEW_PATCH"
+TAG="${RELEASE_MAJOR:-$DEFAULT_MAJOR}.${RELEASE_MINOR:-0}.$NEW_PATCH"
 
 BRANCH=$(echo "$THIS_FILE" | cut -d '.' -f4)
-
-TAG="$NEW_RELEASE_TAG"
 
 echo "New release tag used: $TAG"
 echo "Target branch: $BRANCH"
 
 cat <<EOF >DATA
-**E2e release creation**:
+**E2E release creation**:
 Tag: $TAG
 Branch: $BRANCH
 Commit: $GITHUB_SHA
