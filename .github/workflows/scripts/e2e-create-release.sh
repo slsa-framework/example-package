d2c49e039b8f275a92a423b736f14c8d354e1699
laurentsimon <laurentsimon@google.com>
2022-08-10 17:41:41 +0000

    update

--- .github/workflows/scripts/e2e-create-release.sh
+++ .github/workflows/scripts/e2e-create-release.sh
@@ -31,6 +31,7 @@ if [[ -n "$annotated_tags" ]]; then
     echo "Listing annotated tags"
     tag_list=$(git tag -l "v$default_major*")
     echo git tag -l "v$default_major*"
+    git branch
     echo "tags: $tag_list"
     while read -r line; do
         tag="$line"
