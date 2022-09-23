0b1be8c232daccaf507ab9954b8669b0308957b2
Asra Ali <asraa@google.com>
2022-09-23 15:21:28 -0500

    fix shell error
    Signed-off-by: Asra Ali <asraa@google.com>

--- .github/workflows/scripts/e2e-verify.common.sh
+++ .github/workflows/scripts/e2e-verify.common.sh
@@ -120,8 +120,8 @@ verify_provenance_authenticity() {
     # Assemble artifact args depending on whether this is a container or binary artifact.
     if [[ "$build_type" == "container" ]]; then
         read -ra artifactArg <<<"${CONTAINER}"
-    else if [[ "$build_type" == "gcb" ]]; then
-        read -ra artifactArg <<<"${CONTAINER} ----builder-id=https://cloudbuild.googleapis.com/GoogleHostedWorker"
+    elif [[ "$build_type" == "gcb" ]]; then
+        read -ra artifactArg <<<"${CONTAINER} --builder-id=https://cloudbuild.googleapis.com/GoogleHostedWorker"
     else
         read -ra artifactArg <<<"$($argr "artifact-path") ${BINARY}"
     fi
