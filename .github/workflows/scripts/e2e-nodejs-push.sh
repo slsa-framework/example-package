00c14f47e0c0dcc5146bba252efcc555f9c0009a
Ian Lewis <ianmlewis@gmail.com>
2023-06-07 00:35:13 +0000

    Update node.js workflow_dispatch test
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- .github/workflows/scripts/e2e-nodejs-push.sh
+++ .github/workflows/scripts/e2e-nodejs-push.sh
@@ -92,6 +92,6 @@ if [ "${this_event}" == "workflow_dispatch" ]; then
     this_file=$(e2e_this_file)
     curl -s -X POST -H "Accept: application/vnd.github.v3+json" \
         "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${this_file}/dispatches" \
-        -d "{\"ref\":\"${branch}\"}" \
+        -d "{\"ref\":\"${branch}\",\"inputs\":{\"trigger_build\": true}}" \
         -H "Authorization: token ${token}"
 fi
