936e62294369b5712ee3c8134253d57a9b74969d
Ian Lewis <ianmlewis@gmail.com>
2023-06-07 02:51:52 +0000

    fix auth
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- .github/workflows/scripts/e2e-badges.sh
+++ .github/workflows/scripts/e2e-badges.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# shellcheck source=/dev/null
 source "./.github/workflows/scripts/e2e-utils.sh"
 
 # e2e_update_badge updates a badge in the repo for the given test.
@@ -13,12 +16,27 @@ e2e_update_badge() {
     curl -s -o "${badge_file}" "https://img.shields.io/badge/${this_file//-/--}-${message//-/--}-${color}?logo=github"
 
     if [ -n "$(git status --porcelain)" ]; then
+        # XXX: Need to create a new checkout to authenticate for some reason.
+        gh repo clone "$GITHUB_REPOSITORY" -- -b main
+        repo_name=$(echo "$GITHUB_REPOSITORY" | cut -d '/' -f2)
+        cd "./${repo_name}"
+
+        token=${PAT_TOKEN+$PAT_TOKEN}
+        if [[ -z "$token" ]]; then
+            token=$GH_TOKEN
+        fi
+
         git config --global user.name github-actions
         git config --global user.email github-actions@github.com
+
+        # Set the remote url to authenticate using the token.
+        git remote set-url origin "https://github-actions:${token}@github.com/${GITHUB_REPOSITORY}.git"
 
         git add "${badge_file}"
         git commit -m "Update badge: ${badge_file}" "${badge_file}"
         git push origin main
+
+        cd -
     fi
 }
 
