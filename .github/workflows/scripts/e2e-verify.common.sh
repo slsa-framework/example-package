36afab982c9ff1c7be38b83f5a700a6482353992
laurentsimon <laurentsimon@google.com>
2023-07-20 04:31:57 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- .github/workflows/scripts/e2e-verify.common.sh
+++ .github/workflows/scripts/e2e-verify.common.sh
@@ -112,9 +112,9 @@ e2e_verify_common_buildDefinition_v1() {
     e2e_verify_predicate_v1_buildDefinition_internalParameters "$1" "GITHUB_TRIGGERING_ACTOR_ID" "$TRIGGERING_ACTOR_ID"
     if [[ -n ${CHECKOUT_SHA1:-} ]]; then
         # If the checkout sha was defined, then verify that there is no ref.
-        e2e_verify_predicate_v1_buildDefinition_resolvedDependencies "$1" "[{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY\",\"digest\":{\"gitCommit\":\"$CHECKOUT_SHA1\"}}]"
+        e2e_verify_predicate_v1_buildDefinition_resolvedDependencies0 "$1" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY\",\"digest\":{\"gitCommit\":\"$CHECKOUT_SHA1\"}}"
     else
-        e2e_verify_predicate_v1_buildDefinition_resolvedDependencies "$1" "[{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@$GITHUB_REF\",\"digest\":{\"gitCommit\":\"$GITHUB_SHA\"}}]"
+        e2e_verify_predicate_v1_buildDefinition_resolvedDependencies0 "$1" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@$GITHUB_REF\",\"digest\":{\"gitCommit\":\"$GITHUB_SHA\"}}"
     fi
 }
 
