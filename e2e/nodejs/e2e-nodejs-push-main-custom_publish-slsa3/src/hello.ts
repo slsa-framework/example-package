f427676f38dc86fab834829026377b98523947e4
Ian Lewis <ianlewis@google.com>
2023-08-07 02:23:55 +0000

    Add custom publish e2e test
    Signed-off-by: Ian Lewis <ianlewis@google.com>

---
+++ e2e/nodejs/e2e-nodejs-push-main-custom_publish-slsa3/src/hello.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview A description of this module.  What would someone
+ * new to your team want to know about the code in this file?
+ * (DO NOT SUBMIT as is; replace this comment.)
+ */
+
+export function hello() {
+  console.log("hello world");
+}
---
+++ e2e/nodejs/e2e-nodejs-push-main-custom_publish-slsa3/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview A description of this module.  What would someone
+ * new to your team want to know about the code in this file?
+ * (DO NOT SUBMIT as is; replace this comment.)
+ */
+
+export { hello } from "./hello";
