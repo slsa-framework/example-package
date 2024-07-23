d6d63e2767e08c8c552c04280770beac5eedcec2
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-07-23 17:24:05 +0000

    debug: redirect to stderr
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- .github/workflows/scripts/e2e-verify.common.sh
+++ .github/workflows/scripts/e2e-verify.common.sh
@@ -193,10 +193,10 @@ e2e_set_payload() {
     local this_builder
     this_builder=$(e2e_this_builder)
     if [[ "${this_builder}" == "gcb" ]]; then
-        echo "**** HERE ****"
-        echo "1: $1"
-        echo "2: $2"
-        echo "jq: $(jq -c ".provenance_summary.provenance[0].envelope.payload = \"$(echo "$2" | base64 -w0)\"" <"$1")"
+        echo "**** HERE ****" >&2
+        echo "1: $1" >&2
+        echo "2: $2" >&2
+        echo "jq: $(jq -c ".provenance_summary.provenance[0].envelope.payload = \"$(echo "$2" | base64 -w0)\"" <"$1")" >&2
         jq -c ".provenance_summary.provenance[0].envelope.payload = \"$(echo "$2" | base64 -w0)\"" <"$1"
     else
         jq -c ".payload = \"$(echo "$2" | base64 -w0)\"" <"$1"
